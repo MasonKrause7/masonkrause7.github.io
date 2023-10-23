adc7ba18844033512e75983835342d5d39563131
Mason <47919925+OAOEnder@users.noreply.github.com>
2023-10-23 18:52:49 -0500

    Working on dynamic event rendering

---
+++ events.js
@@ -0,0 +1,21 @@
+var event1 = {
+    title: "2023 Fall Program Presentation and Musical Performance featuring Dr. David Goldblatt, on the works of Julius Chajes",
+    date: "Tuesday, October 17",
+    times: {
+        "5:00PM" : "Reception",
+        "5:30PM" : "Presentation and Performance"
+    },
+    location: "Trahern Auditorium",
+    details: {
+        RSVP : "Please RSVP by Friday, October 13 by calling 931-221-7618",
+    },
+    images : {
+        //image-url : image-caption
+        "/images/apsu-multicolor-logo.jpg" : "The APSU multi color logo",
+        "/images/library-front-sculpture.jpg" : "Sculpture out front of library"
+    }
+}
+var event2 = {
+    title: "Second Event",
+    date: "10/23/2023"
+}
--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-function getQueryParametersAsJSON() {
-    let url = window.location.href;
-    let delimIndex = url.indexOf("?");
-    let args = url.substring(delimIndex+1);
-    let argsArr = args.split("&");
-    let json = {}
-    for(let i = 0; i < argsArr.length; i++){
-        let nameAndVal = argsArr[i].split("=");
-        let name = nameAndVal[0];
-        let val = nameAndVal[1];
-        json[name] = val;
-    }
-    console.log(json);
-    return json;
+
+
+function getIdArg(){
+    const urlArgs = new URLSearchParams(window.location.search);
+    return urlArgs.get('id');
 }
+function renderEventInfo(){
+    const id = getIdArg();
+    const eventObj = window[id];
+    console.log(eventObj);
+    if(eventObj.title){
+        document.getElementById("event-title").innerText = `${eventObj.title}`;
+    }
+}
+
 window.onload = () => {
-    if(window.location.href.indexOf("?") >= 0){
-        args = getQueryParametersAsJSON();
-        if(args.id){
-            document.getElementById(`${args.id}`).style.display="block";
-        }
+    if(window.location.href.includes('event.html?')){
+        renderEventInfo();
     }
 }
