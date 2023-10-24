7d46f5590f8fdb4d01bc9ade980291a2f343e67e
Mason Krause <masongkrause@yahoo.com>
2023-10-23 20:19:24 -0500

    Rendering event details

--- events.js
+++ events.js
@@ -1,21 +1,47 @@
+var currentEventNumber = 3;
 var event1 = {
-    title: "2023 Fall Program Presentation and Musical Performance featuring Dr. David Goldblatt, on the works of Julius Chajes",
-    date: "Tuesday, October 17",
-    times: {
-        "5:00PM" : "Reception",
-        "5:30PM" : "Presentation and Performance"
-    },
-    location: "Trahern Auditorium",
-    details: {
-        RSVP : "Please RSVP by Friday, October 13 by calling 931-221-7618",
-    },
-    images : {
-        //image-url : image-caption
-        "/images/apsu-multicolor-logo.jpg" : "The APSU multi color logo",
-        "/images/library-front-sculpture.jpg" : "Sculpture out front of library"
-    }
-}
+  title: "Dinner on the River: An Evening with Michael Shoulders",
+  date: "April 17, 2009",
+  times: {
+    "11:30 AM - 2 PM": "",
+  },
+  location: "Morgan University Center",
+  details: {
+    Thanks: `Great thanks to those who joined us for the inaugural social event of The Woodward Library Society, Dinner on the River: An Evening with Michael Shoulders.`,
+    Description: `The dinner is open to everyone, regardless of whether or not you are a member of the Society. 
+        Mr. Shoulders is a Clarksville resident, a much sought after speaker, and a nationally known author of children's books, including V is for Volunteer and Count on Us: A Tennessee Number Book.
+        Five of Mr. Shoulders' books signed by the author will be given away as door prizes.
+        For more information, please contact the Library Director's office at 931-221-7618 or email weberj.
+        Pictures from this event can be found in the Gallery. You can also read the The Leaf Chronicle Article about this event.`,
+  },
+  images: {
+    //image-url : image-caption
+  },
+};
 var event2 = {
-    title: "Second Event",
-    date: "10/23/2023"
-}
+  title: "TEL: Tennessee Electronic Library",
+  date: "Tuesday, September 15, 2009",
+  times: {
+    "5:30 PM": "",
+  },
+  location: "F&M Bank, Franklin Room, 50 Franklin Street, Clarksville",
+  details: {
+    Description: `A description and demonstration of the resources that make up TEL: Tennessee Electronic Library, a vast array of online resources made available free of charge to the citizens of Tennessee by the Tennessee State Library & Archives.
+       A reception with hors d'oeuvres and beverages will precede the program, along with a ceremony to celebrate the Society's first donation to the Woodward Library.
+       This event is sponsored by the Woodward Library Society of Austin Peay State University and F&M Bank, and is free of charge for all Woodward Library Society members and prospective members.`,
+  },
+  images: {},
+};
+var event3 = {
+  title: '"The Magic of Poetry, the Poetry of Magic" by Malcolm Glass',
+  date: "Sunday, March 21, 2010",
+  times: {
+    "2:00-4:00 PM": "",
+  },
+  location: "Trahern Theatre, Austin Peay State University",
+  details: {
+    Description: `Refreshments and fellowship following the program.`,
+    RSVP: `Please RSVP by March 18, 2010 to 931-221-7222.`,
+    Note: `This event is for members only. Please see the memberships page for a description of members benefits.`,
+  },
+};
--- script.js
+++ script.js
@@ -1,20 +1,70 @@
+/* DYNAMICALLY RENDERING EVENTS from the events.js file*/
 
-
-function getIdArg(){
-    const urlArgs = new URLSearchParams(window.location.search);
-    return urlArgs.get('id');
+function getIdArg() {
+  const urlArgs = new URLSearchParams(window.location.search);
+  return urlArgs.get("id");
 }
-function renderEventInfo(){
-    const id = getIdArg();
-    const eventObj = window[id];
-    console.log(eventObj);
-    if(eventObj.title){
-        document.getElementById("event-title").innerText = `${eventObj.title}`;
-    }
+function renderEventInfo() {
+  const id = getIdArg();
+  const eventObj = window[id];
+  let eventTitle = document.getElementById("event-title");
+  if (eventObj.title) {
+    eventTitle.innerText = `${eventObj.title}`;
+  } else {
+    eventTitle.innerText = "Untitled Event";
+  }
+  if (eventObj.date) {
+    let eventDate = document.getElementById("event-date");
+    eventDate.innerText = `${eventObj.date}`;
+    eventDate.style.display = "block";
+  }
+  if (eventObj.times) {
+    let eventTimeList = document.getElementById("event-time-list");
+    for (let i = 0; i < Object.keys(eventObj.times).length; i++)
+      eventTimeList.insertAdjacentHTML(
+        "beforeend",
+        `<li><p>${Object.keys(eventObj.times)[i]} ${
+          eventObj.times[i] == undefined ? "" : eventObj.times[i]
+        }</p></li>`
+      );
+    eventTimeList.style.display = "block";
+  }
+  if (eventObj.location) {
+  }
+}
+function renderCurrentEvent() {
+  const currEvent = window[`event${currentEventNumber}`];
+  const currEventList = document.getElementById("current-event-list");
+  currEventList.insertAdjacentHTML(
+    "beforeend",
+    `
+        <li><a href="event.html?id=event${currentEventNumber}">${currEvent.title}</a></li>
+    `
+  );
+}
+function renderPastEvents() {
+  for (let i = 1; i < currentEventNumber; i++) {
+    let obj = window[`event${i}`];
+    const pastEventList = document.getElementById("past-event-list");
+    pastEventList.insertAdjacentHTML(
+      "beforeend",
+      `
+        <li><a href="event.html?id=event${i}">${obj.title}</a></li>
+    `
+    );
+  }
+}
+function renderEventLists() {
+  renderCurrentEvent();
+  renderPastEvents();
 }
 
 window.onload = () => {
-    if(window.location.href.includes('event.html?')){
-        renderEventInfo();
-    }
-}
+  if (window.location.href.includes("event.html?")) {
+    renderEventInfo();
+  } else if (window.location.href.includes("events.html")) {
+    renderEventLists();
+  }
+};
+
+/* END OF DYNAMIC EVENT RENDERING */
