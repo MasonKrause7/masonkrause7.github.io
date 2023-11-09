023aa12bbb1fc063a2f3b5fbfbf2bcf16ec89cfe
Mason <47919925+OAOEnder@users.noreply.github.com>
2023-11-09 13:33:20 -0600

    Initial full site commit

---
+++ events.js
@@ -0,0 +1,214 @@
+//totalNumberOfEvents should match the greatest event_ number in the eventList object.
+//this allows for quick rendering of event list instead of checking for greatest id in eventList
+const totalNumberOfEvents = 7;
+
+//numberOfEventsThatAreCurrent controls how many events will be shown in the "current" section of event list
+//should be used if there are multiple planned upcoming events
+//will render the greatest eventid down to greatestID - numberOfEventsThatAreCurrent, SO
+//new events that are added should always be given an ID that is 1 greater than the previous event.
+const numberOfEventsThatAreCurrent = 2;
+
+
+let eventList = {
+
+  event1: {
+    title: "Dinner on the River: An Evening with Michael Shoulders",
+    season: "2009 Inaugural Event",
+    date: "April 17, 2009",
+    times: {
+      "11:30 AM - 2:00 PM": "",
+    },
+    location: "Morgan University Center",
+    details: {
+      postEventThanks: `Great thanks to those who joined us for the inaugural social event of The Woodward Library Society, Dinner on the River: An Evening with Michael Shoulders.`,
+      description: `The dinner is open to everyone, regardless of whether or not you are a member of the Society.`,
+      speakerDescription: `Mr. Shoulders is a Clarksville resident, a much sought after speaker, and a nationally known author of children's books, including V is for Volunteer and Count on Us: A Tennessee Number Book.
+          Five of Mr. Shoulders' books signed by the author will be given away as door prizes.`,
+      moreInfo: `For more information, please contact the Library Director's office at 931-221-7618.
+          Pictures from this event can be found in the <a href="gallery.html">Gallery</a>. You can also read <a href="https://www.theleafchronicle.com/article/20090501/COMMUNITY/905010315/1086/Woodward+Library+Society+benefits+APSU+campus+library">The Leaf Chronicle Article</a> about this event.`,
+    },
+    images: {
+      //image-url : alt
+    },
+  },
+
+  event2: {
+    title: "TEL: Tennessee Electronic Library",
+    season: "2009 Fall Event",
+    date: "Tuesday, September 15, 2009",
+    times: {
+      "5:30 PM": "",
+    },
+    location: "F&M Bank, Franklin Room, 50 Franklin Street, Clarksville",
+    details: {
+      description: `A description and demonstration of the resources that make up TEL: Tennessee Electronic Library, a vast array of online resources made available free of charge to the citizens of Tennessee by the Tennessee State Library & Archives.
+        A reception with hors d'oeuvres and beverages will precede the program, along with a ceremony to celebrate the Society's first donation to the Woodward Library.
+        This event is sponsored by the Woodward Library Society of Austin Peay State University and F&M Bank, and is free of charge for all Woodward Library Society members and prospective members.`,
+    },
+    images: {},
+  },
+
+  event3: {
+    title: '"The Magic of Poetry, the Poetry of Magic" by Malcolm Glass',
+    season: "2010 Winter Program",
+    date: "Sunday, March 21, 2010",
+    times: {
+      "2:00-4:00 PM": "",
+    },
+    location: "Trahern Theatre, Austin Peay State University",
+    details: {
+      description: `Refreshments and fellowship following the program.`,
+      RSVP: `Please RSVP by March 18, 2010 to 931-221-7222.`,
+      note: `This event is for members only. Please see the memberships page for a description of members benefits.`,
+    },
+    images: {},
+  },
+
+  event4: {
+    title: "<i>The Society Social</i>, An Evening with Wade Bourne",
+    subtitle: "Clarksville's own free-lance outdoor writer and broadcaster",
+    season: "2010 Spring Program",
+    date: "Thursday, April 22, 2010",
+    times: {
+      "5:30 - 7:00 PM": "Moveable Feast",
+      "7:00 PM": "Program",
+    },
+    location:
+      "Customs House Museum and Cultural Center <br> 200 South Second St., Clarksville, TN",
+    details: {
+      price:
+        "$30 per person for members of the Woodward Library Society.<br>$40 per person for non-members.",
+      RSVP: "Attendance is limited, please RSVP to 931-221-7222 before Wednesday, April 14, 2010.",
+    },
+    images: {},
+  },
+
+  event5: {
+    title: '"A Taste of Italia"',
+    subtitle: "featuring Matthew Frank, author of <i>Barolo</i>",
+    season: "2010 Summer Program",
+    date: "Thursday, July 29, 2010",
+    times: {
+      "6:00 PM": "Social Hour",
+      "7:00 PM": "Speaker",
+    },
+    location: "F&M Bank, Franklin Room<br>50 Franklin St., Clarksville",
+    details: {
+      description: "Wine, antipasto and dolce!",
+      price: "Complimentary to members, $20 for non-members.",
+      RSVP: "Reservation required, limited seating. Call 931-221-7618 to reserve your seat.",
+    },
+  },
+
+  event6: {
+    title: '"Visiting Writers Series: Richard Bausch"',
+    subtitle:
+      "Reading from his short story collection, <i>Something Is Out There</i><br>In conjunction with the Center of Excellence for the Creative Arts and <i>Zone 3</i>",
+    season: "2010 Fall Program",
+    date: "Thursday, October 28, 2010",
+    times: {
+      "7:00-7:45 PM": "Dessert Reception",
+      "8:00 PM": "Program commencement",
+    },
+    location:
+      "Dessert Reception will be held at the F.G. Woodward Library.<br>Program will be held at the Morgan University Center, room 303.",
+    details: {
+      note: "<strong>Reception for Sponsor and Benefactor level members</strong>($500 and up)",
+      receptionNotes:
+        "Hosted by President Tim & Mrs. Lee Nicholson Hall<br>5:00-6:30 PM, Thursday, November 11, 2010<br>APSU Pace Alumni Center at Emerald Hills",
+    },
+  },
+
+  event7: {
+    title: '"A Celebration of Scholarship"',
+    subtitle: "Creative and Scholarly Achievements at Austin Peay",
+    season: "2011 Winter Program",
+    date: "Thursday, 17 February, 2011",
+    times: {
+      "7:00 PM": "",
+    },
+    location: "F.G. Woodward Library",
+    details: {
+      description:
+        "A reception to honor the scholarly and creative achievements of APSU faculty, staff, and students.",
+      note: "An exhibit of scholarly creations will be on the 2nd (main) floor of Woodward Library from Feb. 17 through March 5.",
+    },
+    primaryPhoto: "images/library-front-sculpture.jpg",
+    images: {
+      "images/woodwardlibrary-sideshot.jpeg": "Woodward Library Sideshot",
+      "images/apsu-multicolor-logo.jpg": "Apsu Multicolor Logo",
+    },
+  },
+
+  /* TEMPLATE - COPY, PASTE, INCREMENT eventID by one and replace _, fill in values.
+  event_: {
+    title: "",
+    subtitle: "",
+    season: "",
+    date: "",
+    times: {
+      //"<time (e.g. 7:00 PM)>" : "<description(eg meet and greet with snacks)>"
+      "": "",
+    },
+    location: "",
+    details: {
+      //"<detailtype(not displayed)>" : "<value to be displayed>"
+      "" : "",
+    },
+    primaryPhoto: "",
+    images: {
+      //"<src>" : "<alt>"
+      "" : "";
+      
+    }
+  },*/
+};
+
+//getters for event rendering
+export function getEvent(eventID) {
+  return eventList[eventID];
+}
+export function getTotalNumberOfEvents() {
+  return totalNumberOfEvents;
+}
+export function getNumberOfEventsThatAreCurrent() {
+  return numberOfEventsThatAreCurrent;
+}
+
+/* END OF EVENTS */
+/* START OF GALLERIES */
+
+let totalNumberOfGalleries = 2;
+let galleryList = {
+  gallery1: {
+    title:
+      'Woodward Library Society Fall 2012 Program: "The Politics of Memory: Archives, Historic Preservation, and Lessons of the Past,"',
+    subtitle: "by Dr. Susan Curtis, Purdue University, October 18, 2012",
+    images: {
+      //<img url> : <img alt attribute>
+      "/images/hashtagGovs.jpeg": "Random img",
+      "/images/libraryfront1.jpg": "",
+    },
+  },
+  gallery2: {
+    title:
+      "Woodward Library Society 2013 Spring Social: Enter Laughing (But Quietly Please!): Amusing Anecdotes from a Life in Libraries",
+    subtitle:
+      "Featuring Dr. Joe Trahern, Professor Emeritus of English, University of Tennessee April 16, 2013",
+    images: {
+      "/images/woodwardlibrary-sideshot.jpeg":
+        "Side shot of Woodward Library Society",
+      "/images/apsu-multicolor-logo.jpg": "Multicolor logo",
+      "/images/library-front-sculpture.jpg": "Library front sculpture",
+    },
+  },
+  //gallery3: {},
+};
+
+//getters for galleries
+export function getGallery(galleryID) {
+  return galleryList[galleryID];
+}
+export function getTotalNumberOfGalleries() {
+  return totalNumberOfGalleries;
+}
---
+++ script.js
@@ -0,0 +1,280 @@
+/* DYNAMICALLY RENDERING EVENTS from the events.js file*/
+import {
+  getEvent,
+  getTotalNumberOfEvents,
+  getNumberOfEventsThatAreCurrent,
+  getTotalNumberOfGalleries,
+  getGallery,
+} from "./events.js";
+
+//looks for an arg called id and returns its value, returns null otherwise
+function getIdArg() {
+  const urlArgs = new URLSearchParams(window.location.search);
+  const id = urlArgs.get("id");
+  if (id) return id;
+  else return null;
+}
+
+//takes the id passed as query param
+//uses the corresponding event object from events.js to render
+//the event information on the event.html page
+function renderEventInfo(id) {
+  const eventObj = getEvent(id);
+  if (id <= 0 || id > getTotalNumberOfEvents) {
+    window.location.href = "../events.html";
+  }
+  if (eventObj.season) {
+    let season = document.getElementById("event-season");
+    season.insertAdjacentHTML("beforeend", `${eventObj.season}`);
+  }
+  let eventTitle = document.getElementById("event-title");
+  if (eventObj.title) {
+    eventTitle.insertAdjacentHTML("beforeend", `${eventObj.title}`);
+  } else {
+    eventTitle.innerText = "Untitled Event";
+  }
+  eventTitle.style.display = "block";
+
+  if (eventObj.subtitle) {
+    const eventSubTitle = document.getElementById("event-subtitle");
+    eventSubTitle.insertAdjacentHTML("beforeend", eventObj.subtitle);
+  }
+
+  if (eventObj.primaryPhoto) {
+    const eventPrimaryPhoto = document.getElementById("pri-photo");
+    eventPrimaryPhoto.setAttribute("src", eventObj.primaryPhoto);
+  }
+
+  if (eventObj.date) {
+    let eventDate = document.getElementById("event-date");
+    eventDate.insertAdjacentHTML("beforeend", `<p>${eventObj.date}</p>`);
+    eventDate.style.display = "block";
+  }
+  if (eventObj.times) {
+    let eventTimeList = document.getElementById("event-time-list");
+    for (let i = 0; i < Object.keys(eventObj.times).length; i++)
+      eventTimeList.insertAdjacentHTML(
+        "beforeend",
+        `<li><p>${Object.keys(eventObj.times)[i]} ${
+          eventObj.times[Object.keys(eventObj.times)[i]] == ""
+            ? ""
+            : `&#x1F80A ${eventObj.times[Object.keys(eventObj.times)[i]]}`
+        }</p></li>`
+      );
+    eventTimeList.style.display = "block";
+  }
+  if (eventObj.location) {
+    let eventLocation = document.getElementById("event-location");
+    eventLocation.insertAdjacentHTML(
+      "beforeend",
+      `<p>${eventObj.location}</p>`
+    );
+    eventLocation.style.display = "block";
+  }
+  if (eventObj.details) {
+    let eventDetails = document.getElementById("event-details");
+    //iterate through the events details and display them
+    for (let i = 0; i < Object.keys(eventObj.details).length; i++) {
+      let key = Object.keys(eventObj.details)[i];
+      eventDetails.insertAdjacentHTML(
+        "beforeend",
+        `<p>${eventObj.details[key]}</p>`
+      );
+    }
+    eventDetails.style.display = "block";
+  }
+  if (eventObj.images) {
+    let eventImages = document.getElementById("event-images");
+    for (let i = 0; i < Object.keys(eventObj.images).length; i++) {
+      let key = Object.keys(eventObj.images)[i];
+      eventImages.insertAdjacentHTML(
+        "beforeend",
+        `
+        
+        <img src="${key}" alt="${eventObj.images[key]}">
+        
+        `
+      );
+    }
+    eventImages.style.display = "flex";
+    eventImages.style.flexWrap = "wrap";
+    eventImages.style.justifyContent = "space-evenly";
+  }
+}
+
+//finds the most recent event (the highest id event) and starts
+//pulling events backward by one until the
+//id value should match the global variable currentEventNumber;
+//renders that event on the current event list of the events.html page
+function renderCurrentEventList() {
+  const id = getIdArg();
+  let eventNumber = id.substring(id.indexOf("t") + 1);
+  let currEventIndex = getTotalNumberOfEvents();
+  for (let i = 0; i < getNumberOfEventsThatAreCurrent(); i++) {
+    const currEvent = getEvent(`event${currEventIndex}`);
+    const currEventList = document.getElementById("current-event-list");
+    if (currEventIndex == eventNumber) {
+      currEventList.insertAdjacentHTML(
+        "beforeend",
+        `<li>${currEvent.season == undefined ? "" : currEvent.season + "<br>"}${
+          currEvent.title
+        }</li>`
+      );
+    } else {
+      currEventList.insertAdjacentHTML(
+        "beforeend",
+        `
+              <li><a href="events.html?id=event${currEventIndex}">${
+          currEvent.season == undefined ? "" : currEvent.season + "<br>"
+        }${currEvent.title}</a></li>
+              `
+      );
+    }
+    currEventIndex--;
+  }
+}
+
+function clearPastEventList() {
+  const pastEventList = document.getElementById("past-event-list");
+  pastEventList.innerHTML = "";
+}
+
+//start with the event with startIndex and pulls events backward
+function renderSomePastEvents() {
+  clearPastEventList();
+  const id = getIdArg();
+  let eventNumber = id.substring(id.indexOf("t") + 1);
+
+  //numberOfPastEvents represents the starting point to start retreiving events for the pastEventsList
+  let numberOfPastEvents =
+    getTotalNumberOfEvents() - getNumberOfEventsThatAreCurrent();
+
+  const pastEventList = document.getElementById("past-event-list");
+
+  for (let i = numberOfPastEvents; i > numberOfPastEvents - 3; i--) {
+    renderPastEvent(i, eventNumber);
+  }
+  pastEventList.insertAdjacentHTML(
+    "beforeend",
+    `<li id="show-more-button-hoverable"><a id="show-more-button">Show more past events...</a></li>`
+  );
+  const showMoreButton = document.getElementById("show-more-button");
+  showMoreButton.addEventListener("click", renderPastEventList);
+}
+
+function renderPastEvent(eventID, eventNumber) {
+  const pastEventList = document.getElementById("past-event-list");
+  let obj = getEvent(`event${eventID}`);
+  if (eventNumber == eventID) {
+    pastEventList.insertAdjacentHTML(
+      "beforeend",
+      `<li>${obj.season == undefined ? "" : obj.season + "<br>"}${
+        obj.title
+      }</li>`
+    );
+  } else {
+    pastEventList.insertAdjacentHTML(
+      "beforeend",
+      `
+          <li><a href="events.html?id=event${eventID}">${
+        obj.season == undefined ? "" : obj.season + "<br>"
+      }${obj.title}</a></li>
+      `
+    );
+  }
+}
+//finds past events (difference between totalNumberOfEvents and eventsThatAreCurrent)
+//renders each event as a link to its corresponding event.html?id=eventN page.
+function renderPastEventList() {
+  clearPastEventList();
+  const id = getIdArg();
+  let eventNumber = id.substring(id.indexOf("t") + 1);
+  const pastEventList = document.getElementById("past-event-list");
+  //numberOfPastEvents represents the starting point to start retreiving events for the pastEventsList
+  let numberOfPastEvents =
+    getTotalNumberOfEvents() - getNumberOfEventsThatAreCurrent();
+
+  for (let i = numberOfPastEvents; i >= 1; i--) {
+    renderPastEvent(i, eventNumber);
+  }
+  pastEventList.insertAdjacentHTML(
+    "beforeend",
+    `<li id="show-less-button-hoverable"><a id="show-less-button">Show less...</a></li>`
+  );
+  const showLessButton = document.getElementById("show-less-button");
+  showLessButton.addEventListener("click", renderSomePastEvents);
+}
+
+//renders both lists on the events.html page.
+function renderEventLists() {
+  renderCurrentEventList();
+  renderSomePastEvents();
+}
+function renderMostRecentEvent() {
+  window.location.href = `events.html?id=event${getTotalNumberOfEvents()}`;
+}
+//runs automatically on window load
+//if the user clicks on an event,
+// this will route them to event.html and render the events information
+//if the user is on the events.html page,
+// this will render all of the past events and the current event
+window.onload = () => {
+  if (window.location.href.includes("events.html?")) {
+    const eventId = getIdArg();
+    renderEventInfo(eventId);
+    renderEventLists();
+  } else if (window.location.href.includes("events.html")) {
+    renderMostRecentEvent();
+  } else if (window.location.href.includes("gallery.html?")) {
+    //render gallery with id from query param
+    const galleryID = getIdArg();
+    renderSpecificGallery(galleryID);
+    renderGalleryList();
+  } else if (window.location.href.includes("gallery.html")) {
+    renderMostRecentGallery();
+  }
+};
+/* END OF DYNAMIC EVENT RENDERING */
+/* BEGIN DYNAMIC GALLERY RENDERING */
+
+function renderMostRecentGallery() {
+  window.location.href = `gallery.html?id=gallery${getTotalNumberOfGalleries()}`;
+}
+function renderGalleryList() {
+  const galId = getIdArg();
+  let id = galId.substring(galId.indexOf("y") + 1);
+  const otherGalList = document.getElementById("gallery-list");
+  for (let i = getTotalNumberOfGalleries(); i >= 1; i--) {
+    if (i != id) {
+      otherGalList.insertAdjacentHTML(
+        "beforebegin",
+        `<li><a href="gallery.html?id=gallery${i}">${
+          getGallery(`gallery${i}`).title
+        }</a></li>`
+      );
+    } else {
+      otherGalList.insertAdjacentHTML(
+        "beforebegin",
+        `<li>${getGallery(`gallery${i}`).title}</li>`
+      );
+    }
+  }
+}
+function renderSpecificGallery(id) {
+  let galObj = getGallery(id);
+  const title = document.getElementById("displayed-gallery-title");
+  title.insertAdjacentHTML("beforeend", `${galObj.title}`);
+  const subtitle = document.getElementById("displayed-gallery-subtitle");
+  subtitle.insertAdjacentHTML("beforeend", `${galObj.subtitle}`);
+  const imageList = document.getElementById("displayed-gallery-images");
+  for (let i = 0; i < Object.keys(galObj.images).length; i++) {
+    imageList.insertAdjacentHTML(
+      "beforeend",
+      `<li><img src="${Object.keys(galObj.images)[i]}" alt="${
+        galObj.images[Object.keys(galObj.images)[i]]
+      }"></li>`
+    );
+  }
+}
+
+/* END OF DYNAMIC GALLERY RENDERING */
